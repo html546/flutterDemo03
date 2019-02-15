b67062fdc733b2f48c08f2730e6948d84d061174
chengjiang <15712859926@163.com>
2019-02-15 15:22:29 +0800

    四个动画效果

--- lib/custome_router.dart
+++ lib/custome_router.dart
@@ -5,7 +5,7 @@ class CustomeRouter extends PageRouteBuilder {
 
   CustomeRouter(this.widget)
       : super(
-            transitionDuration: Duration(seconds: 2),
+            transitionDuration: Duration(seconds: 1),
             pageBuilder: (
               BuildContext context,
               Animation<double> animation1,
@@ -19,10 +19,45 @@ class CustomeRouter extends PageRouteBuilder {
               Animation<double> animation2,
               Widget child,
             ) {
-              return FadeTransition(
+              //渐隐渐现的路由动画效果
+              /* return FadeTransition(
                 opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
                   parent: animation1,
                   curve: Curves.fastOutSlowIn,
+                )),
+                child: child,
+              ); */
+              // 缩放的动画效果
+              /* return ScaleTransition(
+                scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+                  parent: animation1,
+                  curve: Curves.fastOutSlowIn,
+                )),
+                child: child,
+              ); */
+              //旋转+缩放动画效果
+              /* return RotationTransition(
+                turns: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+                  parent: animation1,
+                  curve: Curves.fastOutSlowIn,
+                )),
+                child: ScaleTransition(
+                  scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+                    parent: animation1,
+                    curve: Curves.fastOutSlowIn,
+                  )),
+                  child: child,
+                ),
+              ); */
+
+              // 左右滑动路由动画
+              return SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(-1.0, 0.0),
+                  end: Offset(0.0, 0.0),
+                ).animate(CurvedAnimation(
+                  parent:animation1,
+                  curve:Curves.elasticIn,
                 )),
                 child: child,
               );
